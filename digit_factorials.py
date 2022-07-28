d26d507dbdb2edcee2e97801f87cb1a077552605
QueFact <mujdebugra@gmail.com>
2022-07-28 10:51:24 +0300

    More problems

---
+++ digit_factorials.py
@@ -0,0 +1,19 @@
+
+def get_factorial(num):
+    factorial = 1
+    for count in range(1, num + 1):
+        factorial *= count
+    return factorial
+
+print(get_factorial(9))
+
+curious_numbers = []
+total = 0
+for number in range(3, 100000):
+    for digit in str(number):
+        total += get_factorial(int(digit))
+    if number == total:
+        curious_numbers.append(number)
+    total = 0
+
+print(sum(curious_numbers))
